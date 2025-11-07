dc8f60af4e2483c52171be514e7681137edb3783
Random0utlander <ricardo.addc.branco@gmail.com>
2025-11-07 15:42:05 +0000

    New Shadders Class

---
+++ README.txt
@@ -0,0 +1,6 @@
+This project is following the https://learnopengl.com/ to learn OpenGL
+
+Commands:
+
+To build from CMake just execute run.sh --> ./run.sh
+To rebuild first clean using --> ./clean.sh
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include "config.h"
 #include "triangle_mesh.h"
+#include "shaders.h"
 
 using namespace std;
-
-//Prototypes
-unsigned int make_shader(const string& vertex_filepath, const string& fragment_filepath);
-unsigned int make_module(const string& filepath, unsigned int module_type);
-//
 
 int main(){
 
@@ -38,7 +34,7 @@ int main(){
 
     TriangleMesh* triangle = new TriangleMesh();
 
-    unsigned int shader =make_shader(
+    Shader shader = Shader(
         "../src/shaders/vertex.txt",
         "../src/shaders/fragments.txt"
     );
@@ -49,97 +45,14 @@ int main(){
         glfwPollEvents();
 
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shader);
+        shader.use();
         triangle->draw();
 
         glfwSwapBuffers(window);
     }
     
-    glDeleteProgram(shader);
+    shader.~Shader();
     delete triangle;
     glfwTerminate();
     return 0;
 }
-
-unsigned int make_shader(const string& vertex_filepath, const string& fragment_filepath){
-    vector<unsigned int> modules;
-    modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
-    modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
-
-    unsigned int shader = glCreateProgram();
-    for (unsigned int shaderModule : modules)
-    {
-        glAttachShader(shader, shaderModule);
-    }
-    glLinkProgram(shader);
-
-    int success;
-    glGetProgramiv(shader, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        char errorLog[1024];
-        glGetShaderInfoLog(shader,  1024, NULL, errorLog);
-        cout << "Shader Linking Error:\n" << errorLog << endl;
-    }
-
-    for(unsigned int shaderModule : modules)
-    {
-        glDeleteShader(shaderModule);
-    }
-
-    return shader;
-}
-
-unsigned int make_module(const string& filepath, unsigned int module_type){
-    /*
-    ifstream file;
-    stringstream bufferedlines;
-    string line;
-
-    file.open(filepath);
-    while (getline(file, line))
-    {
-        bufferedlines << line << "\n";
-    }
-    string shaderSource = bufferedlines.str();
-    const char* shaderSrc = shaderSource.c_str();
-    bufferedlines.str("");
-    file.close();
-*/
-    std::string shaderCode;
-    std::ifstream ShaderFile;
-    // ensure ifstream objects can throw exceptions:
-    ShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-    try 
-    {
-        // open files
-        ShaderFile.open(filepath);
-        std::stringstream ShaderStream;
-        // read file's buffer contents into streams
-        ShaderStream << ShaderFile.rdbuf();
-        // close file handlers
-        ShaderFile.close();
-        // convert stream into string
-        shaderCode   = ShaderStream.str();
-    }
-    catch(std::ifstream::failure e)
-    {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-    }
-    const char* ShaderCode = shaderCode.c_str();
-
-    unsigned int shaderModule = glCreateShader(module_type);
-    glShaderSource(shaderModule, 1, &ShaderCode, NULL);
-    glCompileShader(shaderModule);
-
-    int success;
-    glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        char errorLog[1024];
-        glGetShaderInfoLog(shaderModule,  1024, NULL, errorLog);
-        cout << "Shader Module Compilation Error:\n" << errorLog << endl;
-    }
-
-    return shaderModule;
-} 
